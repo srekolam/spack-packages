5caf60aadbd698c10d3bc880ab2aa1b9d3c5f0f3
Sreenivasa Murthy Kolam <sreenivasamurthy.kolam@amd.com>
2026-09-01 11:18:40 +0530

    bump up the version for therock-10.0 release

--- repos/spack_repo/builtin/packages/comgr/package.py
+++ repos/spack_repo/builtin/packages/comgr/package.py
@@ -32,7 +32,7 @@ class Comgr(CMakePackage):
             # For versions >= 7.13, use therock-{major}.{minor} tag format
             url = "https://github.com/ROCm/llvm-project/archive/refs/tags/therock-{0}.{1}.tar.gz"
             return url.format(version[0], version[1])
-
+    version("10.0.0", sha256="92dcfe3d2e1ca52b460c7f168d8d74e47cd17370fa8bdffb919174115f94ab0f")
     version("7.14.0", sha256="db365c1f0ab500eeee04a990d29d79a6bb667874f8069f7b69920ca62c352d2f")
     version("7.13.0", sha256="49f5e3d743b51aae87807cd44b00c2aa9fdeb7e78e2fa84f21d69b8be573e161")
     version("7.2.3", sha256="6239fa0c72b150cf0a325676264d3030a67389dec4fca7103f563a70c2b70114")
@@ -102,6 +102,7 @@ class Comgr(CMakePackage):
         "7.2.3",
         "7.13.0",
         "7.14.0",
+        "10.0.0",
     ]:
         # llvm libs are linked statically, so this *could* be a build dep
         depends_on(f"llvm-amdgpu@{ver}", when=f"@{ver}")
